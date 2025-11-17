5a8a26601f93a37a25fdb074b150f34838e58360
Blaise29Pars <blaze29pars@gmail.com>
2025-11-17 13:28:39 -0500

    feat: implemented resources.py into controllers

---
+++ api/controllers/resources.py
@@ -0,0 +1,100 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response
+from sqlalchemy.exc import SQLAlchemyError
+from ..models import resources as model
+
+
+def create(db: Session, request):
+    new_item = model.Resource(
+        item=request.item,
+        amount=request.amount,
+    )
+
+    try:
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+        return new_item
+    except SQLAlchemyError as e:
+        db.rollback()
+        error = str(getattr(e, "orig", e))
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+
+
+def read_all(db: Session):
+    try:
+        return db.query(model.Resource).all()
+    except SQLAlchemyError as e:
+        error = str(getattr(e, "orig", e))
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+
+
+def read_one(db: Session, item_id: int):
+    try:
+        item = db.query(model.Resource).filter(item_id == model.Resource.id).first()
+        if not item:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Id not found!"
+            )
+        return item
+
+    except SQLAlchemyError as e:
+        error = str(getattr(e, "orig", e))
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+
+
+def update(db: Session, item_id: int, request):
+    try:
+        item_q = db.query(model.Resource).filter(item_id == model.Resource.id)
+
+        if not item_q.first():
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Id not found!"
+            )
+
+        update_data = request.dict(exclude_unset=True)
+        item_q.update(update_data, synchronize_session=False)
+        db.commit()
+        return item_q.first()
+
+    except SQLAlchemyError as e:
+        db.rollback()
+        error = str(getattr(e, "orig", e))
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+
+
+def delete(db: Session, item_id: int):
+    try:
+        item_q = db.query(model.Resource).filter(item_id == model.Resource.id)
+
+        if not item_q.first():
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Id not found!"
+            )
+
+        item_q.delete(synchronize_session=False)
+        db.commit()
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    except SQLAlchemyError as e:
+        db.rollback()
+        error = str(getattr(e, "orig", e))
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
